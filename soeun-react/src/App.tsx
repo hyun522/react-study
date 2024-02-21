ebb9346fb23b555be46cdd2e5bd809c7de138bf5
sobokki <jso4093@naver.com>
2024-02-21 10:06:48 +0900

    feat: count기능 완성

--- soeun-react/src/App.tsx
+++ soeun-react/src/App.tsx
@@ -1,8 +1,47 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Buttons = styled.div``;
+const StyledButton = styled.button`
+  background-color: #cccccc;
+
+  width: 55px;
+  height: 33px;
+  border-radius: 4px;
+  border: none;
+  margin-left: 10px;
+  &: hover {
+    background-color: #00ffc0;
+  }
+`;
+
 export default function Count() {
+  const [count, setCount] = useState(0);
+  const maxValue = 100;
+  const minValue = -100;
+  const onIncrease = () => {
+    if (count < maxValue) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
+  const onDecrease = () => {
+    if (count > minValue) {
+      setCount((prevCount) => prevCount - 1);
+    }
+  };
+
   return (
-    <>
-      <button></button>
-      <button></button>
-    </>
+    <Container>
+      <h2> {count}</h2>
+      <Buttons>
+        <StyledButton onClick={onIncrease}>+1</StyledButton>
+        <StyledButton onClick={onDecrease}>-1</StyledButton>
+      </Buttons>
+    </Container>
   );
 }
