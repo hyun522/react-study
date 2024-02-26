b22d34e33b3d92145a1e864ce9d75e8fc5f57d0e
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-02-26 09:23:01 +0900

    feat: counter
    * feat: counter
    
    * fix gitignore
    
    * fix: counter button

--- button-counter/src/App.tsx
+++ button-counter/src/App.tsx
@@ -2,15 +2,25 @@ import { useState } from "react";
 import styled from "styled-components";
 
 const Button = styled.button`
+  margin-right: 1rem;
   font-weight: 500;
   font-size: 2rem;
-  color: #27275b;
+  color: #865a82;
   border-radius: 0.3rem;
-  border: 1px solid #27275b;
+  border: 1px solid #ee9ee6;
+  background-color: rgba(233, 183, 225, 0.515);
 
   &:hover {
-    background-color: #7575cf;
-    outline: #27275b;
+    background-color: #ee9ee67c;
+    outline: #ee9ee6;
+    cursor: pointer;
+  }
+
+  &:disabled {
+    color: #dedada;
+    background-color: #fafafa;
+    border: 1px solid #dadada;
+    cursor: default;
   }
 `;
 
@@ -21,8 +31,12 @@ function App() {
     <>
       <h1>{count}</h1>
 
-      <Button onClick={() => setCount((count) => count + 1)}> + 1 </Button>
-      <Button onClick={() => setCount((count) => count - 1)}> - 1 </Button>
+      <Button disabled={count >= 10} onClick={() => setCount((count) => count + 1)}>
+        + 1
+      </Button>
+      <Button disabled={count === 0} onClick={() => setCount((count) => count - 1)}>
+        - 1
+      </Button>
     </>
   );
 }
