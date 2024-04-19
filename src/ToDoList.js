3496804f7de9e1bd0553f05aa498dfbf42e383ae
정현진 <jhj1004v@naver.com>
2024-04-19 20:32:48 +0900

    UI 구현

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import style from './scss/Todolist.module.scss'
 
-
-function ToDoList(props) {
+function Todolist() {
     return (
-        <div>
-            
+        <div className={style.content}>
+            <p className={style.title}>일정관리</p>
+            <div className={style.putInputButton}>
+                <input className={style.putText} placeholder='할 일을 입력하세요'></input>
+                <button className={style.plusButton}>+</button>
+            </div>
+            {/* 입력할 값이 담길 곳 */}
+            <div className={style.list}>
+
+            </div>
         </div>
     );
 }
 
-export default ToDoList;
+export default Todolist;
