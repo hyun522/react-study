b50fd2323b186a1cdba249a6d235eeb8b657f32b
정현진 <jhj1004v@naver.com>
2024-04-19 20:31:40 +0900

    STYLE : Todolist 컴포넌트 이름 변경에 따른 수정

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './Calculator.js'
 import Counter from './Counter.js';
-import ToDoList from './ToDoList.js'
+import Todolist from './Todolist.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +12,7 @@ root.render(
           <Route path='/' element={<App />}/>
           <Route path="/counter" element={<Counter />} />
           <Route path="/calculator" element={<Calculator />} />
-          <Route path="/todolist" element={<ToDoList />} />
+          <Route path="/todolist" element={<Todolist />} />
         </Routes>
     </BrowserRouter>
 );
