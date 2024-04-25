22994a968a6b6d2dd09b63c7d77a5bad1252049c
sobokki <jso4093@naver.com>
2024-04-25 10:04:53 +0900

    todoList

--- soeun-react/src/MyRouter.tsx
+++ soeun-react/src/MyRouter.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Count from "./functions/counter.tsx";
 import Calculator from "./functions/calculator.tsx";
-
+import TodoList from "./functions/todoList.tsx";
 const MyRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/functions/counter" element={<Count />} />
         <Route path="/functions/calculator" element={<Calculator />} />
+           <Route path="/functions/todoList" element={<TodoList />} />
       </Routes>
     </Router>
   );
