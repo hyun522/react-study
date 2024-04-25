6ffd86020e6b7a066b21662c052b8d1a0de937f5
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-04-25 11:53:51 +0900

    feat: to do list 구현

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,5 +1,60 @@
+import { useCallback, useRef, useState } from "react";
+import Input from "./components/Input";
+import TodoList from "./components/List";
+
 function App() {
-  return <div>gggg</div>;
+  const [todos, setTodos] = useState<{ id: number; text: string; checked: boolean }[]>([]);
+  const [value, setValue] = useState("");
+  const nextId = useRef(1);
+
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    return setValue(inputValue);
+  };
+
+  const handleDeleteTask = useCallback(
+    (id: number) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos]
+  );
+
+  const handleEditTask = useCallback(
+    (id: number, newText: string) => {
+      setTodos(todos.map((todo) => (todo.id === id ? { ...todo, text: newText } : todo)));
+    },
+    [todos]
+  );
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (value.trim() !== "") {
+      setTodos([
+        ...todos,
+        {
+          id: nextId.current,
+          text: value,
+          checked: false,
+        },
+      ]);
+      nextId.current++;
+      setValue("");
+    }
+  };
+  const handleCheckTask = useCallback(
+    (id: number) => {
+      setTodos(todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo)));
+    },
+    [todos]
+  );
+
+  return (
+    <>
+      <Input value={value} onChange={handleInputValue} onSubmit={handleSubmit} />
+      <TodoList todos={todos} onChecked={handleCheckTask} onEdit={handleEditTask} onDelete={handleDeleteTask} />
+    </>
+  );
 }
 
 export default App;
