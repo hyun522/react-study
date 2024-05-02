9607c6cc1543cf70d3cd555c9197bf7a995448ed
hyun522 <134246845+hyun522@users.noreply.github.com>
2024-05-02 10:20:28 +0900

    Hyun/todolist 기능 구현 (#12)
    * CHORE : node-sass 설치
    
    * STYLE : Todolist 컴포넌트 이름 변경에 따른 수정
    
    * DESIGIN : UI 구현
    
    * UI 구현
    
    * DESIGN : UI 구현 및 초기화 css 는 global 설정
    
    * FEAT : todolist 기능 구현
    
    * STYLE : test 하기 위해 생성
    
    * FIX : checkbox 버그 해결

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './Calculator.js'
 import Counter from './Counter.js';
-import ToDoList from './ToDoList.js'
+import Todolist from './Todolist.js'
+import Practice from './Practice.js';
+import './scss/global.module.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +14,8 @@ root.render(
           <Route path='/' element={<App />}/>
           <Route path="/counter" element={<Counter />} />
           <Route path="/calculator" element={<Calculator />} />
-          <Route path="/todolist" element={<ToDoList />} />
+          <Route path="/todolist" element={<Todolist />} />
+          <Route path="/practice" element={<Practice />} />
         </Routes>
     </BrowserRouter>
 );
