c1473715e7d756d466611646476c9f2d9a97bee3
sobokki <jso4093@naver.com>
2024-05-13 10:58:49 +0900

    Feat: 별점 컴포넌트 구현

--- soeun-react/src/MyRouter.tsx
+++ soeun-react/src/MyRouter.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Count from "./functions/counter.tsx";
 import Calculator from "./functions/calculator.tsx";
 import TodoList from "./functions/todoList.tsx";
+import Star from "./functions/star.tsx";
 const MyRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/functions/counter" element={<Count />} />
         <Route path="/functions/calculator" element={<Calculator />} />
-           <Route path="/functions/todoList" element={<TodoList />} />
+        <Route path="/functions/todoList" element={<TodoList />} />
+        <Route path="/functions/star" element={<Star/>} />
       </Routes>
     </Router>
   );
