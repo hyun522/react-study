fdf54154acf411590a49c305ba183ac5761f6f2d
정 현 진 <jhj1004v@naver.com>
2024-05-14 11:13:22 +0900

    REMOVE : pages폴더로 이동

---
+++ src/pages/Drag.js
@@ -0,0 +1,11 @@
+export default function Drag() {
+  return (
+    <div>
+      <div>item1</div>
+      <div>item2</div>
+      <div>item3</div>
+      <div>item4</div>
+      <div>item5</div>
+    </div>
+  );
+}
